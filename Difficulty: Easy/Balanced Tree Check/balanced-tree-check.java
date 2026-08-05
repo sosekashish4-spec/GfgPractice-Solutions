762781874dbd1598261bc39615fdb15fa27d1e6d
Kashish <sosekashish4@gmail.com>
2026-08-05 18:39:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Balanced Tree Check/balanced-tree-check.java
@@ -0,0 +1,24 @@
+/* Structure of binary tree node
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}*/
+
+class Solution {
+    public int level(Node root){
+        if(root==null) return 0;
+        return 1+Math.max(level(root.left),level(root.right));
+    }
+    public boolean isBalanced(Node root) {
+        if(root==null) return true;
+         int m=Math.abs(level(root.left)-level(root.right));
+         if(m>1) return false;
+         return isBalanced(root.left) && isBalanced(root.right);
+    }
+}
