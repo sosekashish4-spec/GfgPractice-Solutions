f873aa3f1080361b45f465bb5c5169a8e2b90b80
Kashish <sosekashish4@gmail.com>
2026-08-30 13:59:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Grid Paths from Top to Bottom Corner/grid-paths-from-top-to-bottom-corner.java
+++ Difficulty: Easy/Grid Paths from Top to Bottom Corner/grid-paths-from-top-to-bottom-corner.java
@@ -1,29 +1,23 @@
 class Solution {
-    public void paths(int row,int col,int[][] mat,ArrayList<ArrayList<Integer>>ans,int[][]matrix){
+    public void paths(ArrayList<ArrayList<Integer>>ans,ArrayList<Integer>a,int row,int col,int[][]mat){
         int m=mat.length;
         int n=mat[0].length;
-        if(row==m-1 && col==n-1){
-            ArrayList<Integer>arr=new ArrayList<>();
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    if(matrix[i][j]==1) arr.add(mat[i][j]);
-                }
-            }
-            arr.add(mat[m-1][n-1]);
-            ans.add(arr);
+        if(mat[row][col]==mat[m-1][n-1]){
+            ArrayList<Integer>copy=new ArrayList<>(a);
+            copy.add(mat[m-1][n-1]);
+            ans.add(copy);
             return;
         }
-        if(row>=m || col>=n) return;
-        matrix[row][col]=1; //mark
-        paths(row,col+1,mat,ans,matrix); //calls
-        paths(row+1,col,mat,ans,matrix);
-        matrix[row][col]=0; //unmark
-        return;
+        
+        a.add(mat[row][col]);
+        if(row<m-1) paths(ans,a,row+1,col,mat); //down
+        if(col<n-1) paths(ans,a,row,col+1,mat); //right
+        a.remove(a.size()-1);
     }
     public ArrayList<ArrayList<Integer>> allPaths(int[][] mat) {
-        int[][]matrix=new int[mat.length][mat[0].length];
-        ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
-        paths(0,0,mat,ans,matrix);
-        return ans;
+         ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
+         ArrayList<Integer>a=new ArrayList<>();
+         paths(ans,a,0,0,mat);
+         return ans;
     }
 }
