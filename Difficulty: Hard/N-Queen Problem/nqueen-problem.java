7aec8fcc88fc208ca3464c1e323952285ec87a1c
Kashish <sosekashish4@gmail.com>
2026-08-30 14:49:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.java
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.java
@@ -1,63 +1,41 @@
 class Solution {
-    public boolean safeplaced(int row, int col,char[][] chessBoard) {
-        int n=chessBoard.length;
-         //upwards
-         for(int i=0;i<n;i++){
-            if(chessBoard[i][col]=='Q') return false;
-         }
-
-         // left-diagonal
-         int i=row-1;
-         int j=col-1;
-         while(i>=0 && j>=0){
-            if(chessBoard[i][j]=='Q') return false;
-            i--;
-            j--;
-         }
-
-         // right diagonal
-         i=row-1;
-         j=col+1;
-         while(i>=0 && j<n){
-            if(chessBoard[i][j]=='Q') return false;
-            i--;
-            j++;
-         }
-         return true;
-    }
+    public boolean valid(int row,int col,int n,boolean[][]grid){
+        for(int i=0;i<col;i++) if(grid[row][i]) return false;
+        
+        for(int i=0;i<row;i++) if(grid[i][col]) return false;
     
-    private void nqueen(int row, char[][] chessBoard,ArrayList<ArrayList<Integer>>ans) {
-        int n=chessBoard.length;
-        ArrayList<Integer>arr;
+        int k=col-1;
+        int j=row-1;
+        while(k>=0 && j>=0) if(grid[j--][k--]) return false;
+        
+        k=col+1;
+        j=row-1;
+        while(k<n && j>=0) if(grid[j--][k++]) return false;
+        
+        return true;
+    }
+    public void queen(ArrayList<ArrayList<Integer>>ans,ArrayList<Integer>sol,int row,int n ,boolean[][]grid){
         if(row==n){
-            arr=new ArrayList<>();
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(chessBoard[i][j]=='Q') arr.add(j+1);
-                }
-            }
-            ans.add(arr);
+            ArrayList<Integer>a=new ArrayList<>(sol);
+            ans.add(a);
             return;
         }
-
-        for(int col=0;col<n;col++){
-            if(safeplaced(row,col,chessBoard)){
-                chessBoard[row][col]='Q';   //mark
-                nqueen(row+1, chessBoard,ans);  //recursion
-                chessBoard[row][col]='-';   //unmark
+        for(int i=0;i<n;i++){
+            if(!valid(row,i,n,grid)) continue;
+            else{
+                sol.add(i+1);
+                grid[row][i]=true;
+                queen(ans,sol,row+1,n,grid);
+                sol.remove(sol.size()-1);
+                grid[row][i]=false;
             }
         }
     }
-    
     public ArrayList<ArrayList<Integer>> nQueen(int n) {
-        char [][]chessBoard=new char[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                chessBoard[i][j]='-';
-            }
-        }
-        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
-        nqueen(0,chessBoard,ans);
-        return ans;
+         ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
+         ArrayList<Integer>sol=new ArrayList<>();
+         boolean[][]grid=new boolean[n][n];
+         queen(ans,sol,0,n,grid);
+         return ans;
     }
 }
